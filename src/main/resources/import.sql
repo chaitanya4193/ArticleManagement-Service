acadefc58421bd4c6901ac71c98bdc2110a0580a
Chaitanya Naik <chaitanya4193@gmail.com>
2017-09-03 22:24:37 +0530

    Added insert sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (id, title, category, user_name) VALUES(1, 'Spring Data','Using spring data JPA', 'cnaik');
+INSERT INTO article (id, title, category, user_name) VALUES(2, 'REST','Using REST controller', 'chaitanya4193');
+INSERT INTO article (id, title, category, user_name) VALUES(3, 'MYSQL','Using MYSQL db', 'Chaitanya Naik');
